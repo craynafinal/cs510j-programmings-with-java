ae36de32ca615a230c20564add3ebb031787124e
jsl <jsl@pdx.edu>
2016-07-03 22:30:56 -0700

    TextDumper and TextParser classes added

---
+++ apptbook/src/main/java/edu/pdx/cs410J/jsl/TextDumper.java
@@ -0,0 +1,13 @@
+package edu.pdx.cs410J.jsl;
+
+import edu.pdx.cs410J.AbstractAppointmentBook;
+import edu.pdx.cs410J.AppointmentBookDumper;
+
+import java.io.IOException;
+
+public class TextDumper implements AppointmentBookDumper {
+    @Override
+    public void dump(AbstractAppointmentBook abstractAppointmentBook) throws IOException {
+
+    }
+}
---
+++ apptbook/src/main/java/edu/pdx/cs410J/jsl/TextParser.java
@@ -0,0 +1,12 @@
+package edu.pdx.cs410J.jsl;
+
+import edu.pdx.cs410J.AbstractAppointmentBook;
+import edu.pdx.cs410J.AppointmentBookParser;
+import edu.pdx.cs410J.ParserException;
+
+public class TextParser implements AppointmentBookParser {
+    @Override
+    public AbstractAppointmentBook parse() throws ParserException {
+        return null;
+    }
+}
