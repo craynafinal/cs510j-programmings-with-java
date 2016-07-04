4f5aef3bd140113fc1d885a73f5fcdc3508ca03a
jsl <jsl@pdx.edu>
2016-07-04 00:26:53 -0700

    dump logic implemented

--- apptbook/src/main/java/edu/pdx/cs410J/jsl/TextDumper.java
+++ apptbook/src/main/java/edu/pdx/cs410J/jsl/TextDumper.java
@@ -3,11 +3,59 @@ package edu.pdx.cs410J.jsl;
 import edu.pdx.cs410J.AbstractAppointmentBook;
 import edu.pdx.cs410J.AppointmentBookDumper;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
 public class TextDumper implements AppointmentBookDumper {
+
+    private String filename = null;
+
+    public TextDumper(String filename) {
+        this.filename = filename;
+    }
+
+    public String getFileName() {
+        return filename;
+    }
+
     @Override
     public void dump(AbstractAppointmentBook abstractAppointmentBook) throws IOException {
+        if (abstractAppointmentBook instanceof AppointmentBook) {
+            dumpToFile((AppointmentBook)abstractAppointmentBook);
+        }
+    }
 
+    private void dumpToFile(AppointmentBook appointmentBook) throws IOException {
+        File file = new File(filename);
+        FileWriter fw = new FileWriter(file);
+        PrintWriter pw = new PrintWriter(fw);
+
+        String ownerName = appointmentBook.getOwnerName();
+        List<Appointment> listOfAppointments = appointmentBook.getAppointments();
+
+        pw.println("<appointmentbook>");
+        pw.println("  <owner>");
+        pw.println("    " + ownerName);
+        pw.println("  </owner>");
+        pw.println("</appointmentbook>");
+
+        for (Appointment app: listOfAppointments) {
+            pw.println("<appointment>");
+            pw.println("  <description>");
+            pw.println("    " + app.getDescription());
+            pw.println("  </description>");
+            pw.println("  <begintime>");
+            pw.println("    " + app.getBeginTimeString());
+            pw.println("  </begintime>");
+            pw.println("  <endtime>");
+            pw.println("    " + app.getEndTimeString());
+            pw.println("  </endtime>");
+            pw.println("</appointment>");
+        }
+
+        pw.close();
     }
 }
--- apptbook/src/test/java/edu/pdx/cs410J/jsl/TextDumperTest.java
+++ apptbook/src/test/java/edu/pdx/cs410J/jsl/TextDumperTest.java
@@ -6,4 +6,13 @@ import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TextDumperTest {
+    @Test
+    public void shouldSetFileNameCorrectly() {
+        AppointmentBook appointmentBook = new AppointmentBook("owner name");
+        appointmentBook.addAppointment(new Appointment("desc1", "begintime1", "endtime1"));
+        appointmentBook.addAppointment(new Appointment("desc2", "begintime2", "endtime2"));
+
+        TextDumper textDumper = new TextDumper("file.txt");
+        assertThat(textDumper.getFileName(), is(equalTo("file.txt")));
+    }
 }
