414fec111348f9f2932dd0ad121c2d992157a864
jsl <jsl@pdx.edu>
2016-07-09 22:02:05 -0700

    documentation of ParseToken class added

--- apptbook/src/main/java/edu/pdx/cs410J/jsl/ParseToken.java
+++ apptbook/src/main/java/edu/pdx/cs410J/jsl/ParseToken.java
@@ -1,5 +1,14 @@
 package edu.pdx.cs410J.jsl;
 
+/**
+ * This enum is designed to be used in <link>TextDumper</link> and <link>TextParser</link> classes.
+ * Because hard coded tokens may cause confusion or mismatch, this <code>ParseToken</code> enum
+ * will be shared among them and data used will be consistant.
+ *
+ * @author    Jong Seong Lee
+ * @version   %I%, %G%
+ * @since     1.0
+ */
 public enum ParseToken {
     APPOINTMENTBOOK(0, "appointmentbook"),
     APPOINTMENTBOOK_OWNER(1, "appointmentbook_owner"),
@@ -9,6 +18,17 @@ public enum ParseToken {
     APPOINTMENT_ENDTIME(1, "appointment_endtime");
 
     String token = null;
+
+    /**
+     * This constructor takes a level of hierarchy and a string of a token.
+     * The top hierarchy level is 0, and increases by 1 every time the hierarchy goes down.
+     * When the hierarchy value is 0, it attaches "--" before the token value.
+     * Everytime the level increases, the number of dashes increases as well.
+     * For example, "---" will be used for the level 1.
+     *
+     * @param level a level of hierarchy
+     * @param token a specific name of a token
+     */
     ParseToken(int level, String token) {
         StringBuilder dash = new StringBuilder();
         for (int i = 0; i < level + 2; i++) {
@@ -17,6 +37,10 @@ public enum ParseToken {
         this.token = dash.toString() + token;
     }
 
+    /**
+     * Returns the constructed string of the token.
+     * @return a token in string format
+     */
     public String getToken() {
         return token;
     }
