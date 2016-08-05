adcae7e5a6656b8c4d38dc9154afdd33d79721be
jsl <jsl@pdx.edu>
2016-08-05 10:23:22 -0700

    proxy test fixed

--- apptbook-gwt/src/it/java/edu/pdx/cs410J/jsl/AppointmentBookServiceSyncProxyIT.java
+++ apptbook-gwt/src/it/java/edu/pdx/cs410J/jsl/AppointmentBookServiceSyncProxyIT.java
@@ -1,6 +1,7 @@
 package edu.pdx.cs410J.jsl;
 
 import com.gdevelop.gwt.syncrpc.SyncProxy;
+import edu.pdx.cs410J.jsl.client.Appointment;
 import edu.pdx.cs410J.jsl.client.AppointmentBook;
 import edu.pdx.cs410J.jsl.client.AppointmentBookService;
 import edu.pdx.cs410J.web.HttpRequestHelper;
@@ -21,17 +22,21 @@ public class AppointmentBookServiceSyncProxyIT extends HttpRequestHelper {
     assertEquals(200, response.getCode());
   }
 
-  /*
   @Test
   public void canInvokeAppointmentBookServiceWithGwtSyncProxy() {
     String moduleName = "apptbook";
     SyncProxy.setBaseURL(this.webAppUrl + "/" + moduleName + "/");
 
+    String owner = "My Owner";
+    String description = "My description";
+    String beginTime = "1/1/2000 11:11 am";
+    String endTime = "2/2/2001 10:10 pm";
+
     AppointmentBookService service = SyncProxy.createSync(AppointmentBookService.class);
-    int numberOfAppointments = 5;
-    AppointmentBook apptbook = service.createAppointmentBook2(numberOfAppointments);
-    assertEquals("My Owner", apptbook.getOwnerName());
-    assertEquals(numberOfAppointments, apptbook.getAppointments().size());
+    String result = service.createAppointmentBook(owner);
+    assertEquals(owner, result);
+
+    result = service.createAppointment(owner, description, beginTime, endTime);
+    assertEquals(new Appointment(description, beginTime, endTime).toString(), result);
   }
-  */
 }
