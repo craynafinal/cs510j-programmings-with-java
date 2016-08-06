d8f93bd74a3c0da57353e899a3edd30186703fcd
jsl <jsl@pdx.edu>
2016-08-06 10:47:07 -0700

    proxy test updated

--- apptbook-gwt/src/it/java/edu/pdx/cs410J/jsl/AppointmentBookServiceSyncProxyIT.java
+++ apptbook-gwt/src/it/java/edu/pdx/cs410J/jsl/AppointmentBookServiceSyncProxyIT.java
@@ -4,12 +4,15 @@ import com.gdevelop.gwt.syncrpc.SyncProxy;
 import edu.pdx.cs410J.jsl.client.Appointment;
 import edu.pdx.cs410J.jsl.client.AppointmentBook;
 import edu.pdx.cs410J.jsl.client.AppointmentBookService;
+import edu.pdx.cs410J.jsl.client.DateUtility;
 import edu.pdx.cs410J.web.HttpRequestHelper;
 import org.junit.Test;
 
 import java.io.IOException;
 
 import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class AppointmentBookServiceSyncProxyIT extends HttpRequestHelper {
 
@@ -27,16 +30,47 @@ public class AppointmentBookServiceSyncProxyIT extends HttpRequestHelper {
     String moduleName = "apptbook";
     SyncProxy.setBaseURL(this.webAppUrl + "/" + moduleName + "/");
 
-    String owner = "My Owner";
+    String[] owners = { "My Owner", "My Owner 2" };
     String description = "My description";
-    String beginTime = "1/1/2000 11:11 am";
-    String endTime = "2/2/2001 10:10 pm";
+    String[] beginTimes = { "1/1/2000 11:11 am", "1/1/2011 11:11 am" };
+    String[] endTimes = { "2/2/2001 10:10 pm", "2/2/2012 10:10 pm" };
 
+    // add appointment books
     AppointmentBookService service = SyncProxy.createSync(AppointmentBookService.class);
-    String result = service.createAppointmentBook(owner);
-    assertEquals(owner, result);
+    String result = service.createAppointmentBook(owners[0]);
+    assertEquals(owners[0], result);
 
-    result = service.createAppointment(owner, description, beginTime, endTime);
-    assertEquals(new Appointment(description, beginTime, endTime).toString(), result);
+
+    // create an appointment
+    result = service.createAppointment(owners[0], description, beginTimes[0], endTimes[0]);
+    assertEquals(new Appointment(description, beginTimes[0], endTimes[0]).toString(), result);
+
+    result = service.createAppointment(owners[0], description, beginTimes[1], endTimes[1]);
+    assertEquals(new Appointment(description, beginTimes[1], endTimes[1]).toString(), result);
+
+    // checking pretty print all
+    result = service.prettyPrintAll(owners[0]);
+    assertTrue(result, result.contains(owners[0]));
+    assertTrue(result, result.contains(description));
+    assertTrue(result, result.contains(DateUtility.parseDateToStringPrettyPrint(DateUtility.parseStringToDate(beginTimes[0]))));
+    assertTrue(result, result.contains(DateUtility.parseDateToStringPrettyPrint(DateUtility.parseStringToDate(endTimes[0]))));
+    assertTrue(result, result.contains(DateUtility.parseDateToStringPrettyPrint(DateUtility.parseStringToDate(beginTimes[1]))));
+    assertTrue(result, result.contains(DateUtility.parseDateToStringPrettyPrint(DateUtility.parseStringToDate(endTimes[1]))));
+
+    // checking pretty print search
+    result = service.prettyPrintSearch(owners[0], beginTimes[0], endTimes[0]);
+    assertTrue(result, result.contains(owners[0]));
+    assertTrue(result, result.contains(description));
+    assertTrue(result, result.contains(DateUtility.parseDateToStringPrettyPrint(DateUtility.parseStringToDate(beginTimes[0]))));
+    assertTrue(result, result.contains(DateUtility.parseDateToStringPrettyPrint(DateUtility.parseStringToDate(endTimes[0]))));
+    assertFalse(result, result.contains(DateUtility.parseDateToStringPrettyPrint(DateUtility.parseStringToDate(beginTimes[1]))));
+    assertFalse(result, result.contains(DateUtility.parseDateToStringPrettyPrint(DateUtility.parseStringToDate(endTimes[1]))));
+
+
+    result = service.getDumpFileLocation(owners[0]);
+    assertTrue(result, result.contains(owners[0]));
+
+
+    //result = service.createAppointmentBoookByFile(owners[0], owners)
   }
 }
