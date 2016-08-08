a2c522edac14b4b8b90a2eaf6c9b6738dc0c6c67
jsl <jsl@pdx.edu>
2016-08-07 20:22:30 -0700

    documents updated

--- apptbook-gwt/src/main/java/edu/pdx/cs410J/jsl/client/AppointmentBookService.java
+++ apptbook-gwt/src/main/java/edu/pdx/cs410J/jsl/client/AppointmentBookService.java
@@ -10,17 +10,63 @@ import java.util.Set;
  */
 @RemoteServiceRelativePath("appointments")
 public interface AppointmentBookService extends RemoteService {
+  /**
+   * Calls the appointment book creation functionality on the server side.
+   *
+   * @param owner
+   * @return
+     */
   public String createAppointmentBook(String owner);
 
+  /**
+   * Calls the appointment creation functionality on the server side.
+   *
+   * @param owner
+   * @param description
+   * @param beginTime
+   * @param endTime
+     * @return
+     */
   public String createAppointment(String owner, String description, String beginTime, String endTime);
 
+  /**
+   * Receives all owner names from the server in <code>Set</code> collection.
+   *
+   * @return
+     */
   public Set<String> getAllOwnerNames();
 
+  /**
+   * Calls the pretty print functionality on the server side.
+   *
+   * @param owner
+   * @return
+     */
   public String prettyPrintAll(String owner);
 
+  /**
+   * Calls the pretty print search functionality on the server side.
+   *
+   * @param owner
+   * @param beginTime
+   * @param endTime
+     * @return
+     */
   public String prettyPrintSearch(String owner, String beginTime, String endTime);
 
+  /**
+   * Get dump file location on the server side.
+   * @param owner
+   * @return
+     */
   public String getDumpFileLocation(String owner);
 
+  /**
+   * Upload the file content to server to restore saved information.
+   *
+   * @param owner
+   * @param fileContent
+   * @return
+     */
   public String restoreAppointmentBook(String owner, String fileContent);
 }
--- apptbook-gwt/src/main/java/edu/pdx/cs410J/jsl/client/AppointmentBookServiceAsync.java
+++ apptbook-gwt/src/main/java/edu/pdx/cs410J/jsl/client/AppointmentBookServiceAsync.java
@@ -8,17 +8,62 @@ import java.util.Set;
  * The client-side interface to the ping service
  */
 public interface AppointmentBookServiceAsync {
+  /**
+   * Calls the appointment book creation functionality on the server side.
+   *
+   * @param owner
+   * @param async
+     */
   void createAppointmentBook(String owner, AsyncCallback<String> async);
 
+  /**
+   * Receives all owner names from the server in <code>Set</code> collection.
+   *
+   * @param async
+     */
   void getAllOwnerNames(AsyncCallback<Set<String>> async);
 
+  /**
+   * Calls the appointment creation functionality on the server side.
+   *
+   * @param owner
+   * @param description
+   * @param beginTime
+   * @param endTime
+     * @param async
+     */
   void createAppointment(String owner, String description, String beginTime, String endTime, AsyncCallback<String> async);
 
+  /**
+   * Calls the pretty print functionality on the server side.
+   *
+   * @param owner
+   * @param async
+     */
   void prettyPrintAll(String owner, AsyncCallback<String> async);
 
+  /**
+   * Calls the pretty print search functionality on the server side.
+   *
+   * @param owner
+   * @param beginTime
+   * @param endTime
+   * @param async
+     */
   void prettyPrintSearch(String owner, String beginTime, String endTime, AsyncCallback<String> async);
 
+  /**
+   * Get dump file location on the server side.
+   * @param owner
+   * @param async
+     */
   void getDumpFileLocation(String owner, AsyncCallback<String> async);
 
+  /**
+   * Upload the file content to server to restore saved information.
+   * @param owner
+   * @param fileContent
+   * @param async
+     */
   void restoreAppointmentBook(String owner, String fileContent, AsyncCallback<String> async);
 }
